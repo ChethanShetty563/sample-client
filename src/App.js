772f6386574c4797e9368c3e932a7ea516b20aac
ChethanShetty563 <chethan563@gmail.com>
2023-06-11 19:26:49 +0530

    Adding the correct url and ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,74 @@
-
+import './App.css';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 function App() {
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    Axios.get('https://master--nimble-daifuku-e5dbb9.netlify.app/.netlify/functions/api/getUsers').then((response) => {
+      setListOfUsers(response.data);
+    });
+  }, []);
+
+  const createUser = () => {
+    Axios.post('https://master--nimble-daifuku-e5dbb9.netlify.app/.netlify/functions/api/createUser', {
+      name,
+      age,
+      username,
+    }).then((response) => {
+      setListOfUsers([
+        ...listOfUsers,
+        {
+          name,
+          age,
+          username,
+        },
+      ]);
+    });
+  };
+
   return (
     <div className="App">
-      <h1>First react Application</h1>
+      <div className="usersDisplay">
+        {listOfUsers.map((user) => {
+          return (
+            <div>
+              <h1>Name: {user.name}</h1>
+              <h1>Age: {user.age}</h1>
+              <h1>Username: {user.username}</h1>
+            </div>
+          );
+        })}
+      </div>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Age..."
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <button onClick={createUser}> Create User </button>
+      </div>
     </div>
   );
 }
